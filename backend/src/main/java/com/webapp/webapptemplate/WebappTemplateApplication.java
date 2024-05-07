a6fb56cd297407133b8b81c528c388c8f6ad9575
Maximilian Spiess <maximilian@spiessh.ch>
2024-05-07 17:48:02 +0200

    initial commit

---
+++ backend/src/main/java/com/webapp/webapptemplate/WebappTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.webapptemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappTemplateApplication.class, args);
+	}
+
+}
